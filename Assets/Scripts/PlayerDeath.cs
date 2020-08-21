6c69257ff3946551294dc05a3bf24b1f4ec81cee
Stratis Dermanoutsos <stratis450@gmail.com>
2020-08-21 20:29:20 +0300

    Player can now die

---
+++ Assets/Scripts/PlayerDeath.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class PlayerDeath : MonoBehaviour
+{
+    void Update()
+    {
+        // If the player falls off the map, die
+        if (gameObject.transform.position.y <= 0) Die();
+    }
+
+    public void Die()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/WallCollision.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class WallCollision : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other) 
+    {
+        // If the player collides with any wall, kill them
+        if (other.name.Equals("Cube - Player"))
+            GameObject.Find("Cube - Player").GetComponent<PlayerDeath>().Die();
+    }
+}
